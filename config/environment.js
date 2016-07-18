79a6e71d236a1a8f1e0b4af3ce3582b4289a42c6
jbandura <jacek.bandura@netguru.pl>
2016-07-18 21:28:02 +0200

    add twbs + setup pods

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'recruguru-front',
+    podModulePrefix: 'recruguru-front/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
